cd29b3f6f5ae3ca71487a9e33c78d7710d4dd8e9
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-12-12 18:35:26 +0000

    memory use improvements + refactor

--- Firmware/Arduino/HeatpumpMonitor_AutoTemp/HeatpumpMonitor_AutoTemp.ino
+++ Firmware/Arduino/HeatpumpMonitor_AutoTemp/HeatpumpMonitor_AutoTemp.ino
@@ -28,19 +28,19 @@
 
 #include <avr/wdt.h>
 
-#define FirmwareVersion = 2.0
+#define FirmwareVersion = 3.0
 #define DEBUG 0
 
 #define RFM69_ENABLE 1
-
 #define OEM_EMON_ENABLE 1
 #define OEM_EMON_ACAC 1
 #define DS18B20_ENABLE 1
-#define KAMSTRUP_ENABLE 1
-#define KAMSTRUP_MODEL 402 // 402, 403, 531 (Sontex)
+
+#define MBUS_ENABLE 1
+#define KAMSTRUP_402 //  or... KAMSTRUP_403, SONTEX_531
+
 #define VFS_ENABLE 1
 #define ELSTER_IRDA_ENABLE 1
-#define MBUS_ENABLE 1
 
 // EmonTH packet
 typedef struct {                                                      // RFM12B RF payload datastructure
@@ -69,18 +69,18 @@ byte numberOfDevices = 0;
 // --------------------------------------------------
 // Grundfos VFS config
 // --------------------------------------------------
-double VFS_maxflow          = 40.0;  // Litres/minute
-double VFS_maxflow_voltage  = 3.5;   // Volts
-double VFS_zeroflow_voltage = 0.25;  // Volts
-double VFS_maxtemp          = 100;   // Celcius
-double VFS_maxtemp_voltage  = 3.5;   // Volts
-double VFS_zerotemp_voltage = 0.5;   // Volts
+const double VFS_maxflow          = 40.0;  // Litres/minute
+const double VFS_maxflow_voltage  = 3.5;   // Volts
+const double VFS_zeroflow_voltage = 0.25;  // Volts
+const double VFS_maxtemp          = 100;   // Celcius
+const double VFS_maxtemp_voltage  = 3.5;   // Volts
+const double VFS_zerotemp_voltage = 0.5;   // Volts
 // --------------------------------------------------
-// Kamstrup MBUS config
+// MBUS
 // --------------------------------------------------
+int mbus_address = -1;                   // Set to 0 for auto scan, (test: check timeout 10ms)
+int mbus_failures = 0;
 
-int kamstrup_mbus_address = -1;                   // Set to 0 for auto scan, (test: check timeout 10ms)
-int kamstrup_failures = 0;
 #include <CustomSoftwareSerial.h>
 CustomSoftwareSerial* customSerial;               // Declare serial
 
@@ -108,28 +108,12 @@ long joules_CT2 = 0;
 long CT1_Wh = 0;
 long CT2_Wh = 0;
 
-// Kamstrup variables
-long KSkWh = 0;
-int volume = 0;
-int ontime = 0;
-int KSflowT = 0;
-int KSreturnT = 0;
-int KSdeltaT = 0;
-int KSheat = 0;
-int maxpower = 0;
-int KSflowrate = 0;
-int maxflow = 0;
-
 // VFS Variables
 double VFSflow = 0;
 double VFStemp = 0;
 
 // pulseCounting
 long pulseCount = 0;
-//unsigned long pulseTime,lastTime; // Used to measure time between pulses
-//double power;
-//int ppwh = 1;                     // pulses per watt hour - found or set on the meter.
-
 bool firstrun = true;
 unsigned long last_reading = 0;
 
@@ -140,8 +124,7 @@ unsigned long msgnum = 0;
 // -------------------------------------------------------------------
 // Elster meter reading
 // -------------------------------------------------------------------
-void meter_reading(unsigned long r)
-{
+void meter_reading(unsigned long r) {
   pulseCount = r;
 }
 
@@ -152,68 +135,11 @@ ElsterA100C meter(meter_reading);
 // The interrupt routine - runs each time a falling edge of a pulse is detected
 // -------------------------------------------------------------------
 //
-void onPulse()
-{
-  // lastTime = pulseTime;
-  // pulseTime = micros();
-  pulseCount++;                                               // count pulse
-  // power = int((3600000000.0 / (pulseTime - lastTime))/ppwh);  // calculate power
+void onPulse() {
+  pulseCount++;
 }
 
-// -------------------------------------------------------------------
-// Phase Decode
-// -------------------------------------------------------------------
-void parse()
-{
-  if (KAMSTRUP_MODEL==402)
-  {
-    byte i=25;
-    KSkWh = decodeval(i); i+=6;
-    volume = decodeval(i); i+=6;
-    ontime = decodeval(i); i+=6;
-    KSflowT = decodeval(i); i+=6;
-    KSreturnT = decodeval(i); i+=6;
-    KSdeltaT = decodeval(i); i+=6;
-    KSheat = decodeval(i)*100; i+=6;
-    maxpower = decodeval(i)*100; i+=6;
-    KSflowrate = decodeval(i); i+=6;
-    maxflow = decodeval(i); i+=6;
-  }
-  
-  if (KAMSTRUP_MODEL==403)
-  {
-    // Locations set here correspond to location as described in datasheet minus 9 bytes
-    // 
-    int offset = 9;
-    KSkWh = decode_4byte_bin(21);
-    // print "Cooling E3:"+str(decode_4byte_bin(30-offset))
-    // print "Energy E8:"+str(decode_4byte_bin(37-offset))
-    // print "Energy E9:"+str(decode_4byte_bin(44-offset))
-    int volume = decode_4byte_bin(50-9);
-    // print "Pulse A:"+str(decode_4byte_bin(57-offset))
-    // print "Pulse B:"+str(decode_4byte_bin(65-offset))
-    int ontime = decode_4byte_bin(71-offset);
-    KSflowT = decode_2byte_bin(83-offset);
-    KSreturnT = decode_2byte_bin(87-offset);
-    KSdeltaT = decode_2byte_bin(91-offset);
-    KSheat = decode_4byte_bin(95-offset);
-    KSflowrate = decode_4byte_bin(107-offset);
-  }
-
-  if (KAMSTRUP_MODEL==531)
-  {
-    Serial.print(",SNXerror:");
-    Serial.print(decode_4byte_bin(27));
-    Serial.print(",SNXenergy:");
-    Serial.print(decode_4byte_bin(39));
-    Serial.print(",SNXvol1:");
-    Serial.print(decode_4byte_bin(45));
-    Serial.print(",SNXvol2:");
-    Serial.print(decode_4byte_bin(59));
-  }
-}
-
-float getTemp(byte* addr){
+float getTemp(){
   byte data[2];
   ds.reset();
   ds.select(addr);
@@ -233,10 +159,6 @@ float getTemp(byte* addr){
 // -------------------------------------------------------------------
 // Decode
 // -------------------------------------------------------------------
-long decodeval(long i) {
-  return bytes[i+2] + (bytes[i+3]<<8) + (bytes[i+4]<<16) + (bytes[i+5]<<24);
-}
-
 long decode_2byte_bin(long i) {
   return bytes[i+0] + (bytes[i+1]<<8);
 }
@@ -253,7 +175,7 @@ void setup() {
   wdt_enable(WDTO_8S);
   
   Serial.begin(115200);
-  if (DEBUG) Serial.println("Startup");
+  if (DEBUG) Serial.println(F("Startup"));
   if (RFM69_ENABLE) rf12_initialize(nodeID, RF_freq, networkGroup);
 
   ct1.voltage(0, 262.0, 1.7);
@@ -270,28 +192,28 @@ void setup() {
 
   wdt_reset();
 
-  if (KAMSTRUP_ENABLE)
+  if (MBUS_ENABLE)
   {
-    if (kamstrup_mbus_address==-1) {
+    if (mbus_address==-1) {
       for (int i=0; i<3; i++) 
       {
-        if (DEBUG) Serial.println("Scanning MBUS ");
+        if (DEBUG) Serial.println(F("Scanning MBUS "));
         mbus_normalize();
         delay(100);
-        kamstrup_mbus_address = mbus_scan();
-        if (kamstrup_mbus_address!=-1) {
-          if (DEBUG) Serial.print("Meter found, address: ");
-          if (DEBUG) Serial.println(kamstrup_mbus_address);
+        mbus_address = mbus_scan();
+        if (mbus_address!=-1) {
+          if (DEBUG) Serial.print(F("Meter found, address: "));
+          if (DEBUG) Serial.println(mbus_address);
           break;
         } else {
-          if (DEBUG) Serial.println("No MBUS meter found");
+          if (DEBUG) Serial.println(F("No MBUS meter found"));
         }
       }
     }
   }
   
   wdt_reset();
-  if (DEBUG) Serial.println("Attached Interrupt");
+  if (DEBUG) Serial.println(F("Attached Interrupt"));
   
   delay(100);
   if (!ELSTER_IRDA_ENABLE) {
@@ -327,7 +249,7 @@ void loop() {
     int node_id = (rf12_hdr & 0x1F);
     byte n = rf12_len;
     if (DEBUG) {
-      Serial.print("RFM node:");
+      Serial.print(F("RFM node:"));
       Serial.print(node_id);
       Serial.print(" len:");
       Serial.println(n);
@@ -337,11 +259,11 @@ void loop() {
     if (node_id == 23 && n == 12)
     {
       emonth = *(PayloadTH*) rf12_data;
-      Serial.print("THtemp:"); Serial.print(emonth.temp*0.1,1);
-      Serial.print(",THexttemp:"); Serial.print(emonth.temp_external*0.1,1);
-      Serial.print(",THhumidity:"); Serial.print(emonth.humidity*0.1,1);
-      Serial.print(",THbattery:"); Serial.print(emonth.battery*0.1,1);
-      Serial.print(",THpulse:"); Serial.println(emonth.pulsecount);
+      Serial.print(F("THtemp:")); Serial.print(emonth.temp*0.1,1);
+      Serial.print(F(",THexttemp:")); Serial.print(emonth.temp_external*0.1,1);
+      Serial.print(F(",THhumidity:")); Serial.print(emonth.humidity*0.1,1);
+      Serial.print(F(",THbattery:")); Serial.print(emonth.battery*0.1,1);
+      Serial.print(F(",THpulse:")); Serial.println(emonth.pulsecount);
     } else {
       // For all other received nodes send as decoded integers: 10_1:1560,10_2:200...
       for (byte i=0; i<n; i+=2)
@@ -373,67 +295,57 @@ void loop() {
       for(byte i=0; i<8; i++) uid += addr[i];
       
       uids[dsid] = uid;
-      temps[dsid] = getTemp(addr);
+      temps[dsid] = getTemp();
       dsid++;
     }
     numberOfDevices = dsid;
 
     delay(200);
     
-    bool kamstrup_reply_received = false;
+    bool mbus_reply_received = false;
     
-    if (KAMSTRUP_ENABLE)
-    {
-      // 1) KAMSTRUP HEAT METER REQUEST:
-      if (MBUS_ENABLE) {
-        if (DEBUG) Serial.println("mbus_request_data");
-        if (kamstrup_mbus_address>0) {
-          mbus_request_data(kamstrup_mbus_address);
-        } else {
-          if (kamstrup_failures>10) {
-            if (DEBUG) Serial.println("Mbus scan start");
-            kamstrup_mbus_address = mbus_scan();
-            kamstrup_failures = 0;
-            wdt_reset();
-          }
+    // 1) KAMSTRUP HEAT METER REQUEST:
+    if (MBUS_ENABLE) {
+      if (DEBUG) Serial.println(F("mbus_request_data"));
+      if (mbus_address>0) {
+        mbus_request_data(mbus_address);
+      } else {
+        if (mbus_failures>10) {
+          if (DEBUG) Serial.println(F("Mbus scan start"));
+          mbus_address = mbus_scan();
+          mbus_failures = 0;
+          wdt_reset();
         }
-        bid = 0;
-    
-        int kamstrup_timeout = 1000;
-        unsigned long timer_start = millis();
-        while (!kamstrup_reply_received && (millis()-timer_start)<kamstrup_timeout)
-        {
-          if (customSerial->available())
-          {
-            
-            byte val = (byte) customSerial->read();
-        
-            bytes[bid] = val;
-            bid++;
-            
-            if (bytes[0]!=104) {
+      }
+      bid = 0;
+  
+      int mbus_timeout = 1000;
+      unsigned long timer_start = millis();
+      while (!mbus_reply_received && (millis()-timer_start)<mbus_timeout)
+      {
+        if (customSerial->available()) {
+          byte val = (byte) customSerial->read();
+      
+          bytes[bid++] = val;
+          if (bytes[0]!=104) bid = 0;
+          
+          if (bid==3) {
+            if (bytes[1]==bytes[2]) {
+              dlen = bytes[1];
+            } else {
               bid = 0;
             }
-            
-            if (bid==3) {
-              if (bytes[1]==bytes[2]) {
-                dlen = bytes[1];
-              } else {
-                bid = 0;
-              }
-            }
+          }
 
-            if (bid==99) // or 120
-            {
-              kamstrup_reply_received = true;
-              bid = 0;
-            }
+          if (bid==99) { // or 120
+            mbus_reply_received = true;
+            bid = 0;
           }
         }
-    
-        if (kamstrup_reply_received==false) {
-          kamstrup_failures++;
-        }
+      }
+  
+      if (mbus_reply_received==false) {
+        mbus_failures++;
       }
     }
 
@@ -513,47 +425,93 @@ void loop() {
     }
 
     msgnum++;
-    Serial.print("Msg:"); Serial.print(msgnum);
+    Serial.print(F("Msg:")); Serial.print(msgnum);
     if (OEM_EMON_ENABLE)
     {
-        Serial.print(",OEMct1:"); Serial.print(OEMct1);
-        Serial.print(",OEMct2:"); Serial.print(OEMct2);
-        Serial.print(",OEMct1Wh:"); Serial.print(CT1_Wh);
-        Serial.print(",OEMct2Wh:"); Serial.print(CT2_Wh);
+        Serial.print(F(",OEMct1:")); Serial.print(OEMct1);
+        Serial.print(F(",OEMct2:")); Serial.print(OEMct2);
+        Serial.print(F(",OEMct1Wh:")); Serial.print(CT1_Wh);
+        Serial.print(F(",OEMct2Wh:")); Serial.print(CT2_Wh);
     }
     
     if (VFS_ENABLE) {
-      Serial.print(",VFStemp:"); Serial.print(VFStemp,1);
-      Serial.print(",VFSflow:"); Serial.print(VFSflow,1);
+      Serial.print(F(",VFStemp:")); Serial.print(VFStemp,2);
+      Serial.print(F(",VFSflow:")); Serial.print(VFSflow,2);
     }
 
     if (DS18B20_ENABLE) { 
         for(int i=0;i<numberOfDevices; i++)
         {
           if (temps[i]>-50 && temps[i]<80) {
-            Serial.print(",T");
+            Serial.print(F(",T"));
             Serial.print(uids[i]);
-            Serial.print(":");
+            Serial.print(F(":"));
             Serial.print(temps[i]);
           }
         }
     }
-    
-    if (KAMSTRUP_ENABLE && kamstrup_reply_received) {
-      // Parse kamstrup mbus data reply
-      parse();
 
-      if (KAMSTRUP_MODEL!=531) {
-        // Print kamstrup data to serial
-        Serial.print(",KSflowT:"); Serial.print(KSflowT*0.01,2);
-        Serial.print(",KSreturnT:"); Serial.print(KSreturnT*0.01,2);
-        Serial.print(",KSdeltaT:"); Serial.print(KSdeltaT*0.01,2);
-        Serial.print(",KSflowrate:"); Serial.print(KSflowrate);
-        Serial.print(",KSheat:"); Serial.print(KSheat);
-        Serial.print(",KSkWh:"); Serial.print(KSkWh);
-      }
+    if (MBUS_ENABLE && mbus_reply_received) {
+
+      #ifdef KAMSTRUP_402
+        Serial.print(F(",KSkWh:")); 
+        Serial.print(decode_4byte_bin(27));
+        // volume decode_4byte_bin(33)
+        // ontime decode_4byte_bin(39)
+        Serial.print(F(",KSflowT:")); 
+        Serial.print(decode_4byte_bin(45)*0.01,2);
+        Serial.print(F(",KSreturnT:"));
+        Serial.print(decode_4byte_bin(51)*0.01,2);
+        Serial.print(F(",KSdeltaT:")); 
+        Serial.print(decode_4byte_bin(57)*0.01,2);
+        Serial.print(F(",KSheat:")); 
+        Serial.print(decode_4byte_bin(63)*100);
+        // maxpower decode_4byte_bin(70)
+        Serial.print(F(",KSflowrate:"));
+        Serial.print(decode_4byte_bin(75));
+        // maxflow decode_4byte_bin(81)
+      #endif
+      
+      #ifdef KAMSTRUP_403
+        // Locations set here correspond to location as described in datasheet minus 9 bytes
+        byte offset = 9;
+        
+        Serial.print(F(",KSkWh:"));    
+        Serial.print(decode_4byte_bin(21));
+        // Cooling E3 decode_4byte_bin(30-offset)
+        // Energy E8 decode_4byte_bin(37-offset)
+        // Energy E9 decode_4byte_bin(44-offset)
+        // Volume decode_4byte_bin(50-9)
+        // PulseA decode_4byte_bin(57-offset)
+        // PulseB decode_4byte_bin(65-offset)
+        // ontime decode_4byte_bin(71-offset)
+        Serial.print(F(",KSflowT:")); 
+        Serial.print(decode_2byte_bin(83-offset));
+        Serial.print(F(",KSreturnT:"));
+        Serial.print(decode_2byte_bin(87-offset));
+        Serial.print(F(",KSdeltaT:")); 
+        Serial.print(decode_2byte_bin(91-offset));
+        Serial.print(F(",KSheat:"));
+        Serial.print(decode_4byte_bin(95-offset));
+        Serial.print(F(",KSflowrate:"));
+        Serial.print(decode_4byte_bin(107-offset));
+      #endif
+    
+      #ifdef SONTEX_531
+        Serial.print(F(",SNXerror:"));
+        Serial.print(decode_4byte_bin(27));
+        Serial.print(F(",SNXenergy:"));
+        Serial.print(decode_4byte_bin(39));
+        Serial.print(F(",SNXvol1:"));
+        Serial.print(decode_4byte_bin(45));
+        Serial.print(F(",SNXvol2:"));
+        Serial.print(decode_4byte_bin(59));
+      #endif
     }
-    Serial.print(",PulseIRDA:"); Serial.print(pulseCount);
+
+    if (pulseCount>0) {
+      Serial.print(F(",PulseIRDA:")); Serial.print(pulseCount);
+    }
     Serial.println();
     delay(100);
 
